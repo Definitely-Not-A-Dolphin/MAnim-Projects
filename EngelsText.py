bd7eee460864a364cfdcbb1d5c3951c80abfa317
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2025-02-13 22:20:40 +0100

    Assets Done

--- EngelsText.py
+++ EngelsText.py
@@ -10,7 +10,7 @@ def addPosition(list1,list2):
 class Scene1(Scene):
   def construct(self):
     
-    def Ophelia(position):
+    def Ophelia(position,mirror):
       result = VGroup()
       
       #Body
@@ -35,9 +35,14 @@ class Scene1(Scene):
       headCenterPos=addPosition(bodyTopPos,[0,.6,0])
       head=Circle(color=WHITE,radius=.5).move_to(addPosition(position,[0,1.1,0]))
       
+      m=1
+      
+      if(mirror):
+        m=-1
+      
       #Hair
-      hair1Pos=addPosition(headCenterPos,[0.2,0.35,0])
-      hair2Pos=addPosition(headCenterPos,[0.4,-.1,0])
+      hair1Pos=addPosition(headCenterPos,[0.2*m,0.35,0])
+      hair2Pos=addPosition(headCenterPos,[0.4*m,-.1,0])
       
       for k in [.4,.25,.1]:
         result.add(
@@ -80,12 +85,17 @@ class Scene1(Scene):
       #Head
       headCenterPos=addPosition(bodyTopPos,[0,.6,0])
       head=Circle(color=WHITE,radius=.5).move_to(addPosition(position,[0,1.1,0]))
+      
       faceDot1Pos=addPosition(headCenterPos,[0.325,-0.2,0])
       faceDot2Pos=addPosition(headCenterPos,[0.075,-0.2,0])
       faceDot3Pos=addPosition(headCenterPos,[0.05,-0.4,0])
       faceDot4Pos=addPosition(headCenterPos,[0.35,-0.4,0])
       faceLine1=Line(faceDot1Pos,faceDot4Pos,color=WHITE)
       faceLine2=Line(faceDot2Pos,faceDot3Pos,color=WHITE)
+      
+      mouthDot1=Dot(addPosition(headCenterPos,[.15,-.35,0]))
+      mouthDot2=Dot(addPosition(headCenterPos,[.25,-.35,0]))
+      mouthLine=Line(addPosition(headCenterPos,[.15,-.35,0]),addPosition(headCenterPos,[.25,-.35,0]),color=WHITE)
       
       #Hair
       hair1Pos=addPosition(headCenterPos,[0.2,-0.3,0])
@@ -99,6 +109,7 @@ class Scene1(Scene):
                  legLeft, legRight,
                  hair1Diff, headFill,
                  faceLine1, faceLine2,
+                 mouthLine
       )
       return result
     
@@ -108,14 +119,16 @@ class Scene1(Scene):
       #Body
       bodyTopPos = addPosition(position,[0,.6,0])
       bodyBottomPos=addPosition(position,[0,-.6,0])
-      bodyEllipse = Ellipse(height=1.2,width=0.4,color=WHITE).move_to(position)
+      bodyEllipse = Ellipse(height=1.2,width=0.6,color=GRAY,fill_opacity=1).move_to(position)
       
       #Arms
       armCenterPos = addPosition(position,[0,.3,0])
-      armLeftPos = addPosition(position,[-.4,-.2,0])
-      armRightPos = addPosition(position,[.4,-.2,0])
-      armLeft = Line(armLeftPos,armCenterPos,color=WHITE)
-      armRight = Difference(Line(armRightPos,armCenterPos),bodyEllipse,color=WHITE)
+      armLeftPos = addPosition(position,[-.5,-.2,0])
+      armCenterLeftPos = addPosition(armCenterPos,[-.1,0,0])
+      armRightPos = addPosition(position,[.5,-.2,0])
+      armCenterRightPos = addPosition(armCenterPos,[.1,0,0])
+      armLeft = Line(armLeftPos,armCenterLeftPos,color=WHITE)
+      armRight = Line(armRightPos,armCenterRightPos,color=WHITE)
       
       #Legs
       legLeftPos = addPosition(bodyBottomPos,[.4,-0.7,0])
@@ -138,8 +151,61 @@ class Scene1(Scene):
       )
       return result
     
+    def AuntAgony(position):
+      result = VGroup()
+      
+      #Body
+      bodyTopPos = addPosition(position,[0,.6,0])
+      bodyBottomPos=addPosition(position,[0,-.6,0])
+      bodyLine = Line(bodyTopPos,bodyBottomPos)
+      
+      #Arms
+      armCenterPos = addPosition(position,[0,.3,0])
+      armLeftPos = addPosition(position,[-.4,-.2,0])
+      armRightPos = addPosition(position,[.4,-.2,0])
+      armLeft = Line(armLeftPos,armCenterPos,color=WHITE)
+      armRight = Line(armRightPos,armCenterPos,color=WHITE)
+      
+      #Legs
+      legLeftPos = addPosition(bodyBottomPos,[.4,-0.7,0])
+      legRightPos = addPosition(bodyBottomPos,[-.4,-0.7,0])
+      legLeft = Line(legLeftPos,bodyBottomPos,color=WHITE)
+      legRight = Line(legRightPos, bodyBottomPos, color=WHITE)
+      
+      headCenterPos=addPosition(bodyTopPos,[0,.6,0])     
+      hat1=addPosition(headCenterPos,[.4,-.05,0])
+      hat3=addPosition(headCenterPos,[.6,.15,0])
+      hat2=addPosition(headCenterPos,[0,.25,0])
+      hat4=addPosition(headCenterPos,[0.2,.45,0])
+      
+      #Head
+      head=Circle(color=WHITE,radius=.5).move_to(addPosition(position,[0,1.1,0]))
+      temp1=Difference(head,Polygon(addPosition(headCenterPos,[.6,-.2,0]),hat3,hat4,addPosition(headCenterPos,[-.2,.4,0])))
+      
+      #Hat
+      hatgroup=VGroup()
+      hatBrim=Line(addPosition(headCenterPos,[.6,-.2,0]),addPosition(headCenterPos,[-.2,.4,0]))
+      hat=Polygon(hat1,hat2,hat4,hat3, color=WHITE)
+      
+      hatgroup.add(hat,
+                   hatBrim
+      )
+      
+      result.add(temp1,
+                 bodyLine, 
+                 armLeft, armRight,
+                 legLeft, legRight,
+                 hatgroup
+      )
+      return result
+    
     self.wait(1)
     
-    self.play(FadeIn(Hamlet([-1,1,0]),Ophelia([1,1,0]),Polonius([-3,1,0])))
+    self.play(FadeIn(Hamlet([-1,1,0]),
+                     Ophelia([1,1,0], True),
+                     Polonius([-3,1,0]),
+                     AuntAgony([6,-2.5,0])
+                     )
+              )
     
     self.wait(3)
